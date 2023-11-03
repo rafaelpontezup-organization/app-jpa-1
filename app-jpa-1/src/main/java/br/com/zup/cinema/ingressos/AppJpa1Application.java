01fa7817ccca913fc47403d8d99535edaa23cd4c
rafael.ponte@zup.com.br <rafael.ponte@zup.com.br>
2023-11-03 18:45:23 +0000

    stk-cli: create app initial commit.

---
+++ app-jpa-1/src/main/java/br/com/zup/cinema/ingressos/AppJpa1Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.cinema.ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJpa1Application.class, args);
+	}
+
+}
